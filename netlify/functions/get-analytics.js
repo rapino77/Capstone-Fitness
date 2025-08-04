26cced9ab608c152a37307080b761953466986b2
rapino77 <rapino788@gmail.com>
2025-08-04 16:07:33 -0400

    airtable

--- netlify/functions/get-analytics.js
+++ netlify/functions/get-analytics.js
@@ -26,7 +26,7 @@ exports.handler = async (event, context) => {
   try {
     // Configure Airtable
     const base = new Airtable({
-      apiKey: process.env.AIRTABLE_API_KEY
+      token: process.env.AIRTABLE_PERSONAL_ACCESS_TOKEN
     }).base(process.env.AIRTABLE_BASE_ID);
 
     const params = event.queryStringParameters || {};
